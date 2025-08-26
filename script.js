65770b53b82c820ee468698ce8700652fd99f985
Nimish Agarwal <agarwalnimish428@gmail.com>
2025-08-26 16:27:35 +0530

    updated

--- script.js
+++ script.js
@@ -1,81 +1,75 @@
-  let score={
-    win:0,
-    loss:0,
-    tie:0,
-    displayScore:function(){
-      return `wins:${score.win}, losses:${score.loss}, ties:${score.tie}`;
-    }
+let scorestr = localStorage.getItem('score');
+let score = scorestr ? JSON.parse(scorestr) : { win: 0, loss: 0, tie: 0 };
 
-  };  
-  function getcomputerChoice(){
-    let randomNumber=Math.random()*3;
- 
-    if(randomNumber>0 && randomNumber<=1){
-      return 'Bat';
-    }
-    else if(randomNumber<=2){
-        return 'ball';  
-      }
-    else{
-        return 'stump';
-      }
+// Always attach the displayScore method
+score.displayScore = function () {
+  return `wins: ${score.win}, losses: ${score.loss}, ties: ${score.tie}`;
+};
+
+function resetScore() {
+  score = { win: 0, loss: 0, tie: 0 };
+  score.displayScore = function () {
+    return `wins: ${score.win}, losses: ${score.loss}, ties: ${score.tie}`;
+  };
+  localStorage.setItem('score', JSON.stringify(score));
+  document.getElementById('result').innerHTML = "Score reset!";
+}
+
+function getcomputerChoice() {
+  let randomNumber = Math.random() * 3;
+  if (randomNumber > 0 && randomNumber <= 1) {
+    return 'Bat';
+  } else if (randomNumber <= 2) {
+    return 'Ball';
+  } else {
+    return 'Stump';
   }
+}
 
-  function getResult(playerChoice,computerChoice){
-  
-    if(playerChoice === 'Bat'){
-      if(computerChoice === 'ball'){
-        score.win++;
-     return 'you win';
-     
-    }
-    else if(computerChoice ==='Bat'){
+function getResult(playerChoice, computerChoice) {
+  if (playerChoice === 'Bat') {
+    if (computerChoice === 'Ball') {
+      score.win++;
+      return 'you win';
+    } else if (computerChoice === 'Bat') {
       score.tie++;
       return 'match draw';
+    } else {
+      score.loss++;
+      return 'you lose';
     }
-    else{
+  } else if (playerChoice === 'Ball') {
+    if (computerChoice === 'Stump') {
+      score.win++;
+      return 'you win';
+    } else if (computerChoice === 'Ball') {
+      score.tie++;
+      return 'match draw';
+    } else {
+      score.loss++;
+      return 'you lose';
+    }
+  } else {
+    if (computerChoice === 'Bat') {
+      score.win++;
+      return 'you win';
+    } else if (computerChoice === 'Stump') {
+      score.tie++;
+      return 'match draw';
+    } else {
       score.loss++;
       return 'you lose';
     }
   }
-    else if(playerChoice === 'ball'){
-      if(computerChoice === 'stump'){
-         score.win++;
-        return 'you win';
-       
-      }
-      else if(computerChoice === 'ball'){
-         score.tie++;
-        return 'match draw';
-       
-      }
-      else{
-        score.loss++;
-        return 'you lose';
-      }
-    }
-      
-    else{
-      if(computerChoice === 'Bat'){
-        score.win++;
-        return 'you win';
-      }
-      else if(computerChoice === 'stump'){
-        score.tie++;
-        return 'match draw';
-      }
-      else{
-         score.loss++;
-        return 'you lose';
-      }
-    }
-  }
-  function showResult(playerChoice,computerChoice,resultMsg){
-    alert(`you have chosen ${playerChoice} and computer choice is ${computerChoice}\n
-    ${resultMsg}\n
-    Your score is\n
+}
 
-    ${score.displayScore()}`);
-
-  }
-   
+function showResult(playerChoice, computerChoice, resultMsg) {
+  localStorage.setItem('score', JSON.stringify(score));
+  document.getElementById('result').innerHTML = `
+    You have chosen <b>${playerChoice}</b> and computer chose <b>${computerChoice}</b>.<br>
+    <strong>${resultMsg}</strong><br>
+    <hr>
+    <b>Your Score:</b><br>
+    ${score.displayScore()}
+  `;
+}
